cb3a9f8bc550542f8cb36b8a9fb829d82c607e0c
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2014-01-30 23:04:34 +1100

    Fix media queries

--- simon.php
+++ simon.php
@@ -18,35 +18,37 @@ include($root.'includes/header.php');
 
 	<div class="content">
 		<div class="column-b-mobile">
-			<div class="thumbnail-1"><img src="images/flora_h.jpg" width="100%" height="auto"></div>
-			<div class="thumbnail-2"><img src="images/img_0761.jpg" width="100%" height="auto"></div>
-			<div class="thumbnail-3"><img src="images/img_0761.jpg" width="100%" height="auto"></div>
+			<div class="thumbnail-1"><img src="images/flora_h_600.jpg" width="100%" height="auto"></div>
+			<div class="thumbnail-2"><img src="images/boat_600.jpg" width="100%" height="auto"></div>
+			<div class="thumbnail-3"><img src="images/boat_600.jpg" width="100%" height="auto"></div>
 
 		</div>
 		<div class="clear"></div>
 
 		<div class="column-a simon">
-			<p>Perhaps it’s a good thing I can’t paint. If I could, I suspect I might spend my days sketching in 
-			cafes or shut up in the safety of my room with a brush. As it is, that I’m so afflicted, I’ve turned 
-			instead to photography as my expressive medium.</p>
+			<div class="text-block">
+				<p>Perhaps it’s a good thing I can’t paint. If I could, I suspect I might spend my days sketching in 
+				cafes or shut up in the safety of my room with a brush. As it is, that I’m so afflicted, I’ve turned 
+				instead to photography as my expressive medium.</p>
 
-			<p>My attitude is that, in general, photography is the lesser art relative to painting. Although, with 
-			no other reasoning beside my own opinion, I believe a great photographer can aspire to great art in a 
-			way a painter never could.</p>
+				<p>My attitude is that, in general, photography is the lesser art relative to painting. Although, with 
+				no other reasoning beside my own opinion, I believe a great photographer can aspire to great art in a 
+				way a painter never could.</p>
 
-			<p>For it’s one thing to sit in the shade of a tree or hidden in the confines of a studio; yet quite 
-			another to risk the desolate and despised place, the cold, the heat and grime, to capture the beauty 
-			in the lonely thing, the dangerous or forgotten thing.</p>
+				<p>For it’s one thing to sit in the shade of a tree or hidden in the confines of a studio; yet quite 
+				another to risk the desolate and despised place, the cold, the heat and grime, to capture the beauty 
+				in the lonely thing, the dangerous or forgotten thing.</p>
 
-			<p>For me, photography allows the inexpressible facet to be polished, seen and felt.</p>
+				<p>For me, photography allows the inexpressible facet to be polished, seen and felt.</p>
 
-			<p>I hope you feel it too.<br><br><br></p>
+				<p>I hope you feel it too.<br><br><br></p>
 
-			<p>Simon Cordingley</p>
+				<p>Simon Cordingley</p>
+			</div>
 		</div>
 
 		<div class="column-b">
-			<img src="images/flora_v.jpg" class="about-page-image" width="80%" height="auto">
+			<img src="images/flora_v_800.jpg" class="about-page-image" width="80%" height="auto">
 		</div>
 		<div class="clear"></div>
 		
